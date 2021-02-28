11da0784e9e55909302476f396c71a5dbc8c1072
Miłosz <miloszrolla@gmail.com>
2021-03-01 00:37:11 +0100

    Finish the JavaScript Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-  alert(document.querySelector("h1").textContent);
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img")
+  let mainImage  = document.querySelector("#gallery-photo > img")
+  let currentClass = "current"
+
+thumbnails.forEach(function(thumbnail){
+  thumbnail.addEventListener("click",function () {
+    // set clicled image as main image.
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    mainImage.setAttribute("src",newImageSrc)
+    mainImage.setAttribute("alt", thumbnail.dataset.description);
+
+    // Change which image is current.
+    document.querySelector(".current").classList.remove(currentClass);
+    thumbnail.parentNode.classList.add(currentClass);
+
+    // Update imgae info.
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
+
+    title.innerHTML = thumbnail.dataset.title;
+    description.innerHTML = thumbnail.dataset.description;
+  });
+});
+
 }
